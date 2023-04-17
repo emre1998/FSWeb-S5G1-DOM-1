72255a334f4e0e856679c223819b2bbf1af68ae7
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-04-17 21:59:24 +0300

    DOM gorevi tamamlandıı

--- src/index.js
+++ src/index.js
@@ -41,5 +41,55 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
 
 console.log('Proje açıldı!')
 
+const navLinks = document.querySelectorAll('nav a');
+const navItems = ["Servisler", "Ürünler", "Vizyon", "Özellikler", "Hakkımızda", "İletişim"];
 
-/* Kodlar Buradan aşağıya */
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].classList.add('italic');
+  navLinks[i].innerHTML = navItems[i];
+}
+
+document.querySelector('h1').innerHTML = "Bu DOM Mükemmel";
+
+const button = document.querySelector('button');
+button.innerHTML = "Başlayın";
+
+
+const imgs = document.querySelectorAll('img');
+const srcs = ["http://localhost:9000/img/logo.png" ,"http://localhost:9000/img/cta.png", "http://localhost:9000/img/accent.png"];
+imgs.forEach((img, i) => {
+  img.src = srcs[i];
+  
+});
+
+const h4s = document.querySelectorAll('h4');
+const texts = ['Özellikler', 'Hakkında', 'Servisler', 'Ürünler', 'Vizyon','İletişim'];
+h4s.forEach((h4, i) => {
+  h4.textContent = texts[i];
+});
+
+const paragrafMetinleri = [
+  "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.",
+  "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.",
+  "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.",
+  "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.",
+  "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.",
+  "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
+  "+90 (123) 456-7899",
+  "satis@birsirketsitesi.com.tr"
+];
+
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach((p, index) => {
+  p.textContent = paragrafMetinleri[index];
+});
+
+const a = document.querySelector('footer a');
+a.innerHTML = 'Copyright Bir Şirket Sitesi 2022';
+a.classList.add('bold');
+
+
+
+
+
